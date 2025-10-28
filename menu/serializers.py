b8f33957ba1f2ce550bb3a0eeab83e337545d52f
KARRI-LOKESH <karrilokesh108@gmail.com>
2025-10-28 20:45:28 +0530

    collectstat

--- menu/serializers.py
+++ menu/serializers.py
@@ -6,18 +6,33 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+
+# ✅ FIXED HERE: ensures full Cloudinary URL is sent
 class MenuItemSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = MenuItem
         fields = '__all__'
+
+    def get_image(self, obj):
+        try:
+            if obj.image and hasattr(obj.image, 'url'):
+                return obj.image.url  # ✅ Full Cloudinary URL
+        except:
+            return None
+        return None
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
 
+
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
         fields = '__all__'
